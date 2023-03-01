6b42e623ec55dc844689d6642f069b5175178182
Nakadzima <pascal.german@yandex.ru>
2023-03-02 00:16:34 +0600

    changed old ViewSet to new, and something renamed

--- users/views.py
+++ users/views.py
@@ -1,27 +1,20 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
 from rest_framework import generics, status, viewsets
-from rest_framework.exceptions import AuthenticationFailed, ValidationError, NotFound
+from rest_framework.exceptions import (AuthenticationFailed, NotFound,
+                                       ValidationError)
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
-
 from rest_framework_simplejwt.views import TokenVerifyView
 
-from .models import User
-
-from .api.serializers import (
-    UserSerializer, TokenVerifySerializer
-)
-from .api.login_serializers import (
-    PersonalLoginWebSerializer,
-)
-from .api.register_serializers import (
-    RegisterStudentSerializer,
-    RegisterAdminSerializer
-)
-
 from .api.custom_funcs import get_token
-from .permissions import IsSuperUser, IsManager, IsUser
+from .api.login_serializers import PersonalLoginWebSerializer
+from .api.register_serializers import (RegisterAdminSerializer,
+                                       RegisterManagerSerializer)
+from .api.serializers import TokenVerifySerializer, UserSerializer, AdminSerializer, ManagerSerializer
+from .models import User
+from .permissions import IsManager, IsSuperUser, IsUser
 
 sensitive_post_parameters_m = method_decorator(
     sensitive_post_parameters(
@@ -36,14 +29,17 @@ class RegisterAdminView(generics.CreateAPIView):
     queryset = User.objects.all()
 
 
-class RegisterStudentView(generics.CreateAPIView):
-    serializer_class = RegisterStudentSerializer 
+class RegisterManagerView(generics.CreateAPIView):
+    serializer_class = RegisterManagerSerializer
     queryset = User.objects.all()
 
 
 class PersonalLoginWebView(generics.GenericAPIView):
     serializer_class = PersonalLoginWebSerializer
     permission_classes = (AllowAny,)
+
+    def get(self, request):
+        return Response(data={"detail": "НЕФИГ ЩАСТАТЬ!!!"}, status=status.HTTP_200_OK)
 
     def post(self, request):
         email = request.data['email']
@@ -57,25 +53,25 @@ class PersonalLoginWebView(generics.GenericAPIView):
         return get_token(user)
 
 
-class StudentViewSet(viewsets.ModelViewSet):
+class MentorViewSet(viewsets.ModelViewSet):
     permission_classes = [IsSuperUser | IsManager | IsUser]
-    queryset = User.objects.filter(user_type="traveler")
+    queryset = User.objects.filter(user_type="user")
     serializer_class = UserSerializer
-    http_method_names = ['get', 'put', 'patch', 'delete']
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
 
 
 class AdminViewSet(viewsets.ModelViewSet):
     permission_classes = [IsSuperUser | IsManager]
     queryset = User.objects.filter(user_type="admin")
-    serializer_class = UserSerializer
-    http_method_names = ['get', 'put', 'patch', 'delete']
+    serializer_class = AdminSerializer
+    http_method_names = ['get', 'post','put', 'patch', 'delete']
 
 
 class ManagerViewSet(viewsets.ModelViewSet):
     permission_classes = [IsSuperUser | IsManager]
     queryset = User.objects.filter(user_type="manager")
-    serializer_class = UserSerializer
-    http_method_names = ['get', 'put', 'patch', 'delete']
+    serializer_class = ManagerSerializer
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
 
 
 class AllUserViewSet(viewsets.ModelViewSet):
@@ -87,4 +83,3 @@ class AllUserViewSet(viewsets.ModelViewSet):
 
 class TokenVerifyView(TokenVerifyView):
     serializer_class = TokenVerifySerializer
-
