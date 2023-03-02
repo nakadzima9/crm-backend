3a34c2d090568d5109d8ff665152d71d201953fc
Nakadzima <pascal.german@yandex.ru>
2023-03-02 16:11:38 +0600

    changed database to postgresql

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 from pathlib import Path
 from decouple import config, Csv
@@ -82,13 +83,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 SITE_ID = 1
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {'default': dj_database_url.config(
+    default=config("DATABASE_URL"),
+    conn_max_age=600,
+    conn_health_checks=True,
+)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
