ca960bea96c671889f35e4f9242b05590c6927a8
Nakadzima <pascal.german@yandex.ru>
2023-03-05 01:27:16 +0600

    setup admin.py

--- cmsapp/admin.py
+++ cmsapp/admin.py
@@ -1,18 +1,85 @@
 from django.contrib import admin
 
-from .models import *
+from .models import (
+    AdvertisingSource,
+    BlackList,
+    CardStatus,
+    Classroom,
+    Course,
+    Department,
+    Group,
+    GroupStatus,
+    Payment,
+    PaymentMethod,
+    ScheduleType,
+    Student,
+    StudentCard,
+)
 
-admin.site.register(Department)
-admin.site.register(Teacher)
-admin.site.register(GroupStatus)
-admin.site.register(Classroom)
-admin.site.register(Course)
-admin.site.register(ScheduleType)
-admin.site.register(Group)
-admin.site.register(AdvertisingSource)
-admin.site.register(CardStatus)
-admin.site.register(PaymentMethod)
-admin.site.register(Student)
-admin.site.register(StudentCard)
-admin.site.register(Payment)
-admin.site.register(BlackList)
+
+class AdvertisingSourceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class BlackListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student', 'reason', 'added_at',)
+
+
+class CardStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status',)
+
+
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'started_at', 'duration_month',)
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'number', 'students_max', 'status', 'created_at', 'course', 'schedule_type', 'classroom',)
+
+
+class GroupStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status_name',)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'client_card', 'created_at', 'user',)
+
+
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class ScheduleTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name', 'start_time', 'end_time',)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'date_of_birth', 'email', 'phone_number', 'group',)
+
+
+class StudentCardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'card_number', 'note', 'department', 'came_from', 'status', 'laptop', 'payment_method',)
+
+
+
+admin.site.register(AdvertisingSource, AdvertisingSourceAdmin)
+admin.site.register(BlackList, BlackListAdmin)
+admin.site.register(CardStatus, CardStatusAdmin)
+admin.site.register(Classroom, ClassroomAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(GroupStatus, GroupStatusAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(PaymentMethod, PaymentMethodAdmin)
+admin.site.register(ScheduleType, ScheduleTypeAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(StudentCard, StudentCardAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from users.models import User, OTP
+from users.models import User, Mentor, OTP
 
-admin.site.register(User)
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'user_type',)
+
+    def get_form(self, request, obj=User, **kwargs):
+        if obj.user_type == 'mentor':
+            self.exclude = ('password', 'last_login')
+        elif 'password' in self.exclude:
+            self.exclude = ('last_login',)
+        form = super(StaffAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+
+
+class MentorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'department',)
+    exclude = ('password', 'last_login', 'groups', 'user_permissions',)
+
+admin.site.register(User, StaffAdmin)
+admin.site.register(Mentor, MentorAdmin)
 admin.site.register(OTP)
