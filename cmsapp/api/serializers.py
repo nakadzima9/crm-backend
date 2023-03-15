8aa36a0fc876aed9d52ce659475e06f2fb89e8b6
mikhaildev07 <93966383+mikiedev07@users.noreply.github.com>
2023-03-15 19:35:36 +0600

    feat: model change

--- cmsapp/api/serializers.py
+++ cmsapp/api/serializers.py
@@ -81,6 +81,7 @@ class PaymentMethodSerializer(ModelSerializer):
 class StudentSerializer(ModelSerializer):
     department = DepartmentSerializer()
     payment_method = PaymentMethodSerializer()
+    # reason = serializers.SerializerMethodField()
 
     class Meta:
         model = Student
@@ -98,6 +99,7 @@ class StudentSerializer(ModelSerializer):
             "payment_method",
             "status",
             "paid",
+            "reason",
         ]
 
     def validate_phone(self, value):
@@ -106,6 +108,8 @@ class StudentSerializer(ModelSerializer):
     def create(self, validated_data):
         department_data = validated_data.pop("department")["name"]
         payment_method_data = validated_data.pop("payment_method")["name"]
+        # dep = self.object_not_found_validate(Department, department_data)
+        # pay = self.object_not_found_validate(PaymentMethod, payment_method_data)
         try:
             dep = Department.objects.get(name=department_data)
         except ObjectDoesNotExist:
@@ -117,6 +121,35 @@ class StudentSerializer(ModelSerializer):
         student = Student(department=dep, payment_method=pay, **validated_data)
         student.save()
         return student
+
+    def object_not_found_validate(self, obj, name):
+        try:
+            data = obj.objects.get(name=name)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError(f"Object {name} does not exist.")
+        return data
+
+    # def get_reason(self, obj):
+    #     print(obj)
+    #     return Student.objects.filter(reason=1).count()
+    #     # stud = Student.objects.all()
+    #     # i = stud.objects.filter(reason=1).count
+
+    # def update(self, instance, validated_data):
+    #     instance.first_name = validated_data.pop("first_name")
+    #     instance.last_name = validated_data.pop("last_name")
+    #     instance.surname = validated_data.pop("surname")
+    #     instance.notes = validated_data.pop("notes")
+    #     instance.phone = validated_data.pop("phone")
+    #     instance.laptop = validated_data.pop("laptop")
+    #     instance.department = validated_data.pop("department")
+    #     instance.came_from = validated_data.pop("came_from")
+    #     instance.payment_method = validated_data.pop("payment_method")
+    #     instance.status = validated_data.pop("status")
+    #     instance.paid = validated_data.pop("paid")
+    #     instance.reason = validated_data.pop("reason")
+    #     instance.save()
+    #     return instance
 
 
 class StudentOnStudySerializer(ModelSerializer):
