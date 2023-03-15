03df0eebe43af590ae8799b0b84b42dff752695a
Nakadzima <pascal.german@yandex.ru>
2023-03-15 18:39:49 +0600

    updated validate_phone func

--- users/api/custom_funcs.py
+++ users/api/custom_funcs.py
@@ -7,16 +7,20 @@ from django.core import exceptions
 from rest_framework_simplejwt.tokens import RefreshToken
 import datetime
 from rest_framework.response import Response
-
+from users.models import User
 
 def validate_phone(value):
     if value is not None:
+        if not value:
+            return ""
         if not value[1:].isnumeric():
             raise serializers.ValidationError('Phone must be numeric symbols')
         if value[:4] != '+996':
             raise serializers.ValidationError('Phone number should start with +996 ')
         elif len(value) != 13:
             raise serializers.ValidationError("Phone number must be 13 characters long")
+        if User.objects.filter(phone=value).first():
+            raise serializers.ValidationError("This number already exists in the system")
         return value
 
 
