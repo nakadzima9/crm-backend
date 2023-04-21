12d3d32c146b86aed5c6803eb602934a4c1d0c89
mikiedev07 <mikeorlov1993@mail.ru>
2023-04-21 12:05:29 +0600

    fix: fitration in students search

--- cmsapp/utils.py
+++ cmsapp/utils.py
@@ -6,7 +6,8 @@ from django.utils import timezone
 def find_user_by_name(names):
     qs = Student.objects.all()
     for name in names.split():
-        qs = qs.filter(Q(on_request=False) & (Q(first_name__icontains=name) | Q(last_name__icontains=name)))
+        qs = qs.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
+        #qs = qs.filter(Q(on_request=False) & (Q(first_name__icontains=name) | Q(last_name__icontains=name)))
     return qs
 
 
